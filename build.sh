e5828d962b56c18b6fcee5f6fd11bb66efae02d1
Matthieu Fronton <fronton@ekino.com>
2015-01-06 15:48:25 +0100

    dryrun for cleanup too

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ trusty  14.04.1 14.04 latest
 dryrun="$(echo "$1" | sed '/^echo$/b;s/.*//g')"
 
 # Cleanup
-docker rm $(docker ps -aqf status=exited) 2>/dev/null
-docker rmi $(docker images -aqf dangling=true) 2>/dev/null
+$dryrun docker rm $(docker ps -aqf status=exited) 2>/dev/null
+$dryrun docker rmi $(docker images -aqf dangling=true) 2>/dev/null
 
 # Build
 for i in $(find * -maxdepth 1 -type d)
