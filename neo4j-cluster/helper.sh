bf132e5efc277d7896b24d0899ebf46075130f2d
Matthieu Fronton <fronton@ekino.com>
2015-01-26 15:18:55 +0100

    neo4j-cluster: add HA dashboard urls in helper

--- neo4j-cluster/helper.sh
+++ neo4j-cluster/helper.sh
@@ -15,7 +15,7 @@ do
       # Start local DNS server
       echo "==> Starting DNS server"
       docker run --name neodns -h neodns -v $(readlink -f $cnf):/etc/dnsmasq.d -d ekino/dnsmasq:latest
-      localdns=$(docker inspect --format {{.NetworkSettings.IPAddress}} `docker ps -l | awk 'NR!=1{print $1}'`)
+      localdns=$(docker inspect --format {{.NetworkSettings.IPAddress}} neodns)
 
       # From Neo4j manual : minimum 3 nodes is required => either start arbiter or call "run:3"
       echo "==> Starting Neo4j cluster nodes + Registering to DNS server"
@@ -40,8 +40,14 @@ do
       w=45
       echo "==> Waiting ${w}s (cluster warmup)"
       sleep $w
-      set -x
       docker exec -ti $(docker ps -l | awk 'NR!=1{print $1}') curl http://localhost:7474
+
+      # Display webadmin URLs
+      echo "==> Check each node's HA setup and availability using urls below"
+      for i in $(docker ps | grep 7474 | sed -r 's/.*:(.....)->7474.*/\1/')
+      do
+        echo "http://localhost:$i/webadmin/#/info/org.neo4j/High%20Availability/"
+      done
       ;;
     'clear')
       docker kill $(docker ps | awk 'NR!=1{print $1}')
